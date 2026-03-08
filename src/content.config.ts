311d0465f8d63fa8fe90246ce2223357e25a394d
葉桜 <64263971+Leaf-0w0@users.noreply.github.com>
2026-03-08 21:56:24 +0800

    Add blog content and dynamic routing page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const post = defineCollection({
+	// Load Markdown and MDX files in the `src/content/post/` directory.
+	loader: glob({ base: './src/content/post', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -18,4 +18,17 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const spec = defineCollection({
+	// Load Markdown and MDX files in the `src/content/spec/` directory.
+	loader: glob({ base: './src/content/spec', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			heroImage: image().optional(),
+		}),
+});
+
+export const collections = { post, spec };
